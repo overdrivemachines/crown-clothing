7da95edd94ed640f7983b6333642ecdb33ae4a2d
Dipen Chauhan <get.dipen@gmail.com>
2023-09-21 22:03:04 -0700

    adding the signIn comp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import { Route, Routes, Outlet } from "react-router-dom";
-import Navigation from "./routes/navigation/navigation.component";
-import Home from "./routes/home/home.component";
 
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sign-in/sign-in.component";
 
 const Shop = () => {
-  return (
-    <h1>I am the Shop Page</h1>
-  )
-}
+  return <h1>I am the Shop Page</h1>;
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigation />} >
+      <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
-  )
+  );
 };
 
 export default App;
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,6 +1,6 @@
 import { Fragment } from "react";
 import { Outlet, Link } from "react-router-dom";
-import { ReactComponent as CrwnLogo } from "../../assets/crown.svg"
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import "./navigation.styles.scss";
 
 const Navigation = () => {
@@ -15,11 +15,14 @@ const Navigation = () => {
           <Link className="nav-link" to="/shop">
             SHOP
           </Link>
+          <Link className="nav-link" to="/sign-in">
+            SIGN IN
+          </Link>
         </div>
       </div>
       <Outlet />
     </Fragment>
-  )
-}
+  );
+};
 
 export default Navigation;
